c81374d45f8f4f15b4ec11195f791b34a1a82c14
ever <ever@qq.com>
2026-03-07 11:04:28 +0800

    添加实时查询btc价格

--- engine-go/go.mod
+++ engine-go/go.mod
@@ -1,3 +1,12 @@
 module engine-go
 
 go 1.25
+
+require (
+	github.com/adshao/go-binance/v2 v2.8.10 // indirect
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+)
--- engine-go/main.go
+++ engine-go/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/adshao/go-binance/v2"
 )
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	fmt.Println("s =", s)
-
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+	client := binance.NewClient("", "")
+	fmt.Println("🚀 现货监控系统启动...")
+	for {
+		symbol := "BTCUSDT"
+		prices, err := client.NewListPricesService().Symbol(symbol).Do(context.Background())
+		if err != nil {
+			log.Printf("❌ 获取价格失败: %v", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		for _, price := range prices {
+			fmt.Printf("⏰ 时间: %s | 币种: %s | 当前价格: %s\n",
+				time.Now().Format("15:04:05"), symbol, price.Price)
+		}
+		time.Sleep(5 * time.Second)
 	}
 }
